9ff3c35a84c2dc1676d58954191115379fea726b
jianghai <marsandheart@gmail.com>
2019-09-10 23:53:19 +0800

    add jujiao demo proj

---
+++ jujiao-test/precache-manifest.186b894ccee2bf5dd1f846c8494a7c69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ab505b0bb79ba093fbfe70897649fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3873a0864fd809ef2d23",
+    "url": "/static/css/main.171dc5a0.chunk.css"
+  },
+  {
+    "revision": "f10b7c402b64df575c3e",
+    "url": "/static/js/2.c5b15f27.chunk.js"
+  },
+  {
+    "revision": "3873a0864fd809ef2d23",
+    "url": "/static/js/main.df3dbe90.chunk.js"
+  },
+  {
+    "revision": "4c67178d2b931184a253",
+    "url": "/static/js/runtime~main.cb5a4603.js"
+  }
+]);
