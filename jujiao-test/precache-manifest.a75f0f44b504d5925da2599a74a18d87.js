0d87fb070c86afe5d8124a34c0e394d452ea3098
jianghai <marsandheart@gmail.com>
2019-09-10 23:59:20 +0800

    modify jujiao-test

---
+++ jujiao-test/precache-manifest.a75f0f44b504d5925da2599a74a18d87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b60631627cd7acab74983f466adabf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3873a0864fd809ef2d23",
+    "url": "./static/css/main.171dc5a0.chunk.css"
+  },
+  {
+    "revision": "f10b7c402b64df575c3e",
+    "url": "./static/js/2.c5b15f27.chunk.js"
+  },
+  {
+    "revision": "3873a0864fd809ef2d23",
+    "url": "./static/js/main.df3dbe90.chunk.js"
+  },
+  {
+    "revision": "8bd43647946c6cf4cec9",
+    "url": "./static/js/runtime~main.e66fef54.js"
+  }
+]);
--- jujiao-test/service-worker.js
+++ jujiao-test/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.186b894ccee2bf5dd1f846c8494a7c69.js"
+  "./precache-manifest.a75f0f44b504d5925da2599a74a18d87.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
