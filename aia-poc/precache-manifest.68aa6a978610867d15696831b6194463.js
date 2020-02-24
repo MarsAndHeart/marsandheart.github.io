2a2dd4eb546b0f2ce17e596b31e5ded9257adda6
marsandheart <marsandheart@gmail.com>
2020-02-24 15:50:48 +0800

    add aia-poc

---
+++ aia-poc/precache-manifest.68aa6a978610867d15696831b6194463.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1978e5859216187e62af1134313f3f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cfeac22b7cc252eb105a",
+    "url": "./static/css/2.fe82c0a9.chunk.css"
+  },
+  {
+    "revision": "484b8dd6b38ae549c43b",
+    "url": "./static/css/main.42d786ce.chunk.css"
+  },
+  {
+    "revision": "cfeac22b7cc252eb105a",
+    "url": "./static/js/2.ab280752.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "./static/js/2.ab280752.chunk.js.LICENSE"
+  },
+  {
+    "revision": "484b8dd6b38ae549c43b",
+    "url": "./static/js/main.338fee87.chunk.js"
+  },
+  {
+    "revision": "90b02a1bf93c7263811c",
+    "url": "./static/js/runtime-main.cc4221f6.js"
+  },
+  {
+    "revision": "012d6a3e191a53393d2a50546025c276",
+    "url": "./static/media/aia-icon.012d6a3e.jpg"
+  }
+]);
